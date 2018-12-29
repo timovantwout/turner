2c47974d08b7ee68e0b5b6c86b3de42cde3bddd5
Max Limper <max@darmstadt-graphics.com>
2018-12-29 01:20:48 +0100

    First working PoC: configurator loads UI plugin and talks to viewer

---
+++ configurator/index.js
@@ -0,0 +1,211 @@
+var turnerVEC = function()
+{    
+    var that = this;
+    
+    // variable to store & access the plugin data
+    // do not alter the name of this variable - it is used by all the plugins to register themselves
+    this.plugins = {};
+
+    // viewer object that is used by all plugins to manipulate the viewer layout and content
+    this.viewerAPI = null;
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.addPluginArea = function(pluginName)
+    {
+        var toolSelectionElem = document.getElementById("toolSelection");
+        
+        var newPluginAreaButton   = document.createElement("div");
+        newPluginAreaButton.classList.add("toolSelectionButton");
+        
+        toolSelectionElem.appendChild(newPluginAreaButton);
+                
+        var toolsElem = document.getElementById("tools");
+        
+        var newPluginArea = document.createElement("div");
+        newPluginArea.classList.add("pluginArea");
+        newPluginArea.id = "pluginArea_" + pluginName;        
+        
+        toolsElem.appendChild(newPluginArea);        
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.genSpacingHTML = function(pluginUIElemObj)
+    {
+        var spacingElem = document.createElement("div");
+        spacingElem.classList.add("uiSpacingElemContainer");
+        
+        return spacingElem;
+    };
+   
+    //---------------------------------------------------------------------------------------------------------
+      
+    this.genSectionHeadingHTML = function(pluginUIElemObj)
+    {
+        var outerElem = document.createElement("div");
+        outerElem.classList.add("uiHeadingElemContainer");
+        
+        var headingElem = document.createElement("span");
+        
+        headingElem.classList.add("section-heading-text");
+        headingElem.innerText = pluginUIElemObj.text;
+        outerElem.appendChild(headingElem);
+        
+        return outerElem;
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.genExplanationTextHTML = function(pluginUIElemObj)
+    {
+        var outerElem = document.createElement("div");
+        outerElem.classList.add("uiElemContainer");
+        
+        var textElem = document.createElement("span");
+        
+        textElem.classList.add("explanation-text");
+        textElem.innerText = pluginUIElemObj.text;
+        outerElem.appendChild(textElem);
+        
+        return outerElem;
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.genToggleSwitchHTML = function(pluginUIElemObj)
+    {
+        var outerElem = document.createElement("div");
+        outerElem.classList.add("uiElemContainer");
+        
+        var labelTextElem = document.createElement("span");
+        var helpElem      = document.createElement("span");
+        var switchElem    = document.createElement("label");
+        var cbElem        = document.createElement("input");
+        var sliderElem    = document.createElement("span");
+        
+        labelTextElem.classList.add("label-text");
+        labelTextElem.innerText = pluginUIElemObj.labelText;
+        outerElem.appendChild(labelTextElem);
+
+        helpElem.classList.add("help-button");
+        helpElem.title = pluginUIElemObj.tooltipText;
+        outerElem.appendChild(helpElem);
+        
+        switchElem.classList.add("switch");
+        outerElem.appendChild(switchElem);
+        
+        cbElem.type     = "checkbox";
+        cbElem.id       = pluginUIElemObj.id;
+        cbElem.checked  = pluginUIElemObj.initValue;
+        cbElem.onchange = pluginUIElemObj.callback;
+        switchElem.appendChild(cbElem);
+        
+        sliderElem.classList.add("slider");
+        sliderElem.classList.add("round");
+        switchElem.appendChild(sliderElem);
+        
+        return outerElem;
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.addPluginUIElement = function(pluginUIElemObj, pluginUIAreaElem)
+    {
+        switch (pluginUIElemObj.type)
+        {
+            case "spacing":
+                pluginUIAreaElem.appendChild(this.genSpacingHTML(pluginUIElemObj));
+                break;
+            case "heading":
+                pluginUIAreaElem.appendChild(this.genSectionHeadingHTML(pluginUIElemObj));
+                break;
+            case "text":
+                pluginUIAreaElem.appendChild(this.genExplanationTextHTML(pluginUIElemObj));
+                break;
+            case "toggle":
+                pluginUIAreaElem.appendChild(this.genToggleSwitchHTML(pluginUIElemObj));
+                break;
+            default:
+                console.error("Plugin UI element type \"" + pluginUIElemObj.type + "\" is not a known type.");
+                break;
+        }
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.populatePluginArea = function(pluginName)
+    {
+        var plugin           = this.plugins[pluginName];    
+        var pluginUIAreaElem = document.getElementById("pluginArea_" + pluginName);         
+        var pluginUIElemObjs = plugin.uiElements;
+        var i;
+        
+        for (i = 0; i < pluginUIElemObjs.length; ++i)
+        {
+            this.addPluginUIElement(pluginUIElemObjs[i], pluginUIAreaElem);
+        }
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.loadPlugins = function()
+    {
+        var retVal = false;
+        
+        for (var pluginName in this.plugins)
+        {
+            if (this.plugins.hasOwnProperty(pluginName))
+            {
+                // perform plugin-specific initialization
+                retVal = this.plugins[pluginName].init();
+                
+                if (retVal)
+                {
+                    // add the plugin's main button / icon to the UI
+                    this.addPluginArea(pluginName);
+                    
+                    // populate the plugin's area with its own UI elements
+                    this.populatePluginArea(pluginName);
+                    
+                    console.log("Plugin \"" + pluginName + "\" loaded successfully.");
+                }
+                else
+                {
+                    console.error("Error initializing plugin \"" + pluginName + "\".");
+                }
+            }
+        }
+    };
+
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.init = function()
+    {
+        var viewerVersion = "1.0";
+        console.log("This is the turner virtual experience configurator, using turner " + viewerVersion);
+        
+        var viewerFrame  = document.getElementById('viewer')
+        var frameContent = (viewer.contentWindow || viewer.contentDocument);
+        this.viewerAPI   = frameContent;
+        
+        if (this.viewerAPI == null)
+        {
+            console.error("Unable to connect to viewer.");
+            return;
+        }
+        console.log("Turner viewer connected.");
+        
+        this.loadPlugins();
+        console.log("All plugins loaded.");
+    };
+
+    //---------------------------------------------------------------------------------------------------------
+    
+};
+
+// creates the global variable for the turner virtual experience configurator (turner VEC)
+// do not alter the name of this variable - it is used by all the plugins to register themselves,
+// and also in the onload callback of the HTML body
+var turnerVECMain = new turnerVEC();
+
---
+++ configurator/plugins/coreUIDesign/coreUIDesign.js
@@ -0,0 +1,147 @@
+var coreUIDesign = function()
+{
+    //---------------------------------------------------------------------------------------------------------
+    //                                  plugin description / tooltip
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.description = 
+    {
+        "title"   : "UI Design",
+        "tooltip" : "Configure basic 2D UI elements"
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    //                                    plugin UI element callbacks
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.displayCompanyLogoToggled = function(event)
+    {        
+        var toggled = event.target.checked;
+        
+        if (toggled)
+        {
+            //...
+        }
+        else
+        {
+            //...
+        }
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.displayProductLogoToggled = function(event)
+    {        
+        var toggled = event.target.checked;
+        
+        if (toggled)
+        {
+            //...
+        }
+        else
+        {
+            //...
+        }
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.display3DIconToggled = function(event)
+    {        
+        var toggled = event.target.checked;
+        
+        if (toggled)
+        {
+            //...
+        }
+        else
+        {
+            //...
+        }
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.display3DBackgroundToggled = function(event)
+    {        
+        var toggled = event.target.checked;
+        var viewer  = turnerVECMain.viewerAPI;
+               
+        viewer.toggle3DBackground(toggled);
+    };
+    
+    //---------------------------------------------------------------------------------------------------------
+    //                                      plugin UI elements
+    //---------------------------------------------------------------------------------------------------------
+        
+    this.uiElements =
+    [    
+        {
+            "type" : "spacing"
+        },
+        {
+            "type"      : "heading",
+            "text"      : "Branding"
+        },        
+        {
+            "type"      : "text",
+            "text"      : "Configure visibility, image and links for different logos, and drag them around on the canvas."
+        },
+        {
+            "id"          : "coreUIDesign_displayCompanyLogoToggle",
+            "type"        : "toggle",
+            "initValue"   : true,
+            "callback"    : this.displayCompanyLogoToggled,
+            "tooltipText" : "Toggles display of the company logo", 
+            "labelText"   : "Company Logo"
+        },
+        {
+            "id"          : "coreUIDesign_displayProductLogoToggle",
+            "type"        : "toggle",
+            "initValue"   : true,
+            "callback"    : this.displayProductLogoToggled,
+            "tooltipText" : "Toggles display of the product logo", 
+            "labelText"   : "Product Logo"
+        },
+        {
+            "id"          : "coreUIDesign_display3DIconToggle",
+            "type"        : "toggle",
+            "initValue"   : true,
+            "callback"    : this.display3DIconToggled,
+            "tooltipText" : "Toggles display of the \"3D\" indicator", 
+            "labelText"   : "\"3D\" Icon"
+        },
+        {
+            "type" : "spacing"
+        },
+        {
+            "id"   : "coreUIDesign_pageBackgroundSection",
+            "type" : "heading",
+            "text" : "Page Background"
+        },        
+        {
+            "type"      : "text",
+            "text"      : "Configure the canvas background to display 3D scene content, transparent HTML, a single color, a gradient, or an image."
+        },
+        {
+            "id"          : "coreUIDesign_display3DBackgroundToggle",
+            "type"        : "toggle",
+            "initValue"   : true,
+            "callback"    : this.display3DBackgroundToggled,
+            "tooltipText" : "Toggles display of the 3D scene background", 
+            "labelText"   : "3D Scene Background"
+        }
+    ];
+        
+    //---------------------------------------------------------------------------------------------------------
+    
+    this.init = function()
+    {
+        // add temporary HTML elements for snapping guidelines
+        // ...
+        
+        return true;
+    }
+};
+
+turnerVECMain.plugins.coreUIDesign = new coreUIDesign();
