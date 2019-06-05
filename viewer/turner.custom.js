81e4d5b04c881c6ad0b57402759bf6ec9d6f3059
Julian <julian@darmstadt-graphics.com>
2019-06-05 15:21:23 +0200

    - updated turner.custom.js from turner viewer (for turner website preview "only")

---
+++ viewer/turner.custom.js
@@ -0,0 +1,16 @@
+addIsReadyCallback(function(){
+
+setElementLink("company-logo", "https://www.dgg3d.com/");
+setElementLink("product-logo", "https://dgg3d.github.io/turner");
+toggle3DBackground(false);
+setElementBackground("renderCanvas", "none");setSkyboxSharpness(1.0);
+setMinVerticalCameraAngle(-90);
+setMaxVerticalCameraAngle(90);
+setMinZoomFactor(3);
+setMaxZoomFactor(4);
+setPanningSensitivity(0.0);
+setContrast(1.0);
+setExposure(1.0);
+setBloom(0);
+
+});
